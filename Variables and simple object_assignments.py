28b4d3065b99a3211a63539b699dc98a56da1702
Jean Koblavi <akoblavi1234@gmail.com>
2021-10-11 21:23:59 -0400

    Add files via upload

---
+++ Variables and simple object_assignments.py
@@ -0,0 +1,27 @@
+string_firstname= "axel"
+string_lastname= "KOBLAVI"
+string_var= "Hello,"
+print(string_var + " " +string_firstname.upper()+ " " +string_lastname.lower())
+string_allname= "axel"+" "+ "Walsh College Student"
+print("koblavi")
+print(string_allname)
+string_quotes= "\"Start by doing what's necessary ; then do what's possible ; and suddenly you are doing the impossible-Francis of assisi\""
+print(string_quotes)
+string_numericalvalue1= 1.5
+string_numericalvalue2= 2.5
+string_addition= string_numericalvalue1+string_numericalvalue2
+string_substraction= string_numericalvalue1-string_numericalvalue2
+string_multiplication= string_numericalvalue1*string_numericalvalue2
+string_division= string_numericalvalue1/string_numericalvalue2
+print(string_numericalvalue1+string_numericalvalue2)
+print(string_numericalvalue1-string_numericalvalue2)
+print(string_numericalvalue1*string_numericalvalue2)
+print(string_numericalvalue1/string_numericalvalue2)
+sq_root= string_numericalvalue1**string_numericalvalue2
+print(sq_root)
+string_introphrase= "Today is"
+String_monthvariable=" October "
+string_numbervariable= 10
+output_variable= string_introphrase + " " + str(string_numbervariable) + String_monthvariable
+print(output_variable)
+
