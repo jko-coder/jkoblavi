70d6ea1dc92db081e3f0630ddb5466bbd05f8997
Jean Koblavi <akoblavi1234@gmail.com>
2021-10-25 17:44:22 -0400

    Add files via upload

---
+++ Assignment,Expressions and Print.py
@@ -0,0 +1,36 @@
+# life = 'Real love'         Comments 1: Basic form of Assignment
+# print(life)
+
+# soccer, Basketball = 'Sport', 'School'     Comments 2: tuple assigment (positional)
+# print(soccer, Basketball)
+
+# [Fall, Autumn] = ['Halloween', 'leaves fall']    Comments 3: List Assignment (positional)
+# print([Fall, Autumn])
+
+# French = Baguette = 'Breakfast'  Comments 4: Multiple- Target assignment
+# print(French)
+
+# E, F, G, H = 'Alphabet letters'    Comments 5:Sequence assignment, generalized
+# print(E, F, G, H)
+
+
+# Y = print('orange')   Comments 6:print is a function call expression in 3.x
+# print(Y)              But it is coded as an expression statement
+
+# N = [186, 679]       Comments 7:Expression Statements and In-place
+# N.append(345)
+# print(N)
+
+# A = 'The pirates'     Comments 8: Print three objects per defaults
+# B = 76
+# C = ['in the sea']
+# print(A, B, C)
+
+
+
+
+
+
+
+
+
